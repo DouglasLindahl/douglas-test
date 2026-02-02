80e032a22dde0ebaea102ded0f333c2455cb7bc5
DouglasLindahl <douglas.lindahl@gmail.com>
2026-02-02 15:51:44 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     siteName: "Uppsikt",
     images: [
       {
-        url: "https://klokasiffror.se/favicon.png",
+        url: "https://uppsikt.se/favicon.png",
         width: 1200,
         height: 630,
         alt: "Preview image",
